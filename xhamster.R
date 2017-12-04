e1c75cd6a79e6d62e3c165287bb4f3498f1061ae
lacrepressor <abekipnis@yahoo.com>
2017-12-03 23:01:33 -0500

    committing more changes

--- xhamster.R
+++ xhamster.R
@@ -16,11 +16,10 @@ dbWriteTable(db,name="data",row.names=F,overwrite=T,value=bp)
 
 yearuploadsquery='select year(upload_date), count(*), sum(nb_views), sum(nb_comments) from data 
 	where year(upload_date) is not NULL 
-group by year(upload_date) 
-order by year(upload_date);'
+  group by year(upload_date) 
+  order by year(upload_date);'
 res1=dbGetQuery(db,yearuploadsquery) 
 graphics.off()
-
 #plot number of videos uploaded per year 
 plot(res1$year,res$count, type="n")
 lines(res1$year,res$count)
@@ -33,16 +32,14 @@ lines(res1$year,res1$'sum(nb_views)')
 plot(res1$year,res1$'sum(nb_comments)', type='n')
 lines(res1$year,res1$'sum(nb_comments)') 
 
-
-
 #do users who upload more get more comments/views/votes on average?
 uploadsvsviewsq='select d.uploader, count(*) as num, avg(nb_votes) as avg_votes, 
 		avg(nb_comments) as avg_comments,
         avg(nb_views) as avg_views 
         from data as d
-		group by uploader
-		having uploader is not null
-		order by count(*) desc
+	    	group by uploader
+	     	having uploader is not null
+	    	order by count(*) desc
         limit 1000;'
 res2=dbGetQuery(db,uploadsvsviewsq) 
 graphics.off()
@@ -52,3 +49,76 @@ library(car)
 scatterplot(res2$num,res2$avg_votes) 
 scatterplot(res2$num,res2$avg_comments)
 scatterplot(res2$num,res2$avg_views)
+
+
+#day of the week uploads
+dayq='select dayofweek(upload_date), count(*) from data 
+	where dayofweek(upload_date) is not NULL 
+  group by dayofweek(upload_date) 
+  order by dayofweek(upload_date);'
+res3=dbGetQuery(db,dayq)
+plot(res3$`dayofweek(upload_date)`,res3$`count(*)`, type='n')
+lines(res3$`dayofweek(upload_date)`,res3$`count(*)`) 
+
+#month uploads
+monthq='select month(upload_date), count(*) from data 
+	where month(upload_date) is not NULL 
+  group by month(upload_date) 
+  order by month(upload_date);'
+res4=dbGetQuery(db,monthq)
+plot(res4$`month(upload_date)`, res4$`count(*)`, type='n')
+lines(res4$`month(upload_date)`,res4$`count(*)`)
+
+#top posting channels
+topchannelsq='select data.channels, count(*)
+	from data
+  inner join categories on data.channels like categories.channels
+  group by data.channels
+  order by count(*) desc;'
+res5=dbGetQuery(db,topchannelsq)
+plot(res5$`count(*)`)
+
+#length preference by channel
+lenprefq='select d.channels,
+  avg(nb_views)/avg(runtime/60) as length_preference
+  from data as d
+  inner join categories on d.channels like categories.channels
+  group by d.channels
+  order by avg(nb_views)/avg(runtime/60) desc;'
+res6=dbGetQuery(db,lenprefq)
+plot(res6$length_preference)
+plot(res6$`avg(nb_comments)`)
+
+#avg number of comments by channel
+commchannelq='select d.channels, avg(nb_comments) as avg_comments
+  from data as d
+  inner join categories on d.channels like categories.channels
+  group by d.channels
+  order by avg(nb_comments) desc'
+res7=dbGetQuery(db,commchannelq)
+plot(res7$avg_comments)
+
+#avg number of views by channel
+viewchannelq='select d.channels, avg(nb_views) as avg_views
+  from data as d
+  inner join categories on d.channels like categories.channels
+  group by d.channels
+  order by avg(nb_views) desc'
+res8=dbGetQuery(db,viewchannelq)
+plot(res8$avg_views)
+
+#posts by day of year
+dayofyearq="select dayofyear(str_TO_DATE(upload_date, '%Y-%d-%m')) as day_of_year, count(*) from data
+	where dayofyear(str_TO_DATE(upload_date, '%Y-%d-%m')) is not NULL
+group by dayofyear(str_TO_DATE(upload_date, '%Y-%d-%m'))
+order by dayofyear(str_TO_DATE(upload_date, '%Y-%d-%m'));"
+res9=dbGetQuery(db,dayofyearq)
+plot(res9$day_of_year,res9$`count(*)`)
+
+#avg runtime by day of year
+runtimedayofyearq="select dayofyear(str_TO_DATE(upload_date, '%Y-%d-%m')) as day_of_year, avg(runtime) from data
+	where dayofyear(str_TO_DATE(upload_date, '%Y-%d-%m')) is not NULL
+group by dayofyear(str_TO_DATE(upload_date, '%Y-%d-%m'))
+order by dayofyear(str_TO_DATE(upload_date, '%Y-%d-%m'));"
+res10=dbGetQuery(db,runtimedayofyearq)
+plot(res10$day_of_year,res10$`avg(runtime)`)
